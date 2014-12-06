a1ad421e43272127f119f4c8b33496cee0143bc6
Francisco Gutierrez <3108939382c@gmail.com>
2014-12-06 15:57:16 -0800

    Added GridPoint class

--- game.py
+++ game.py
@@ -3,3 +3,38 @@
 #CST 205 - Final
 
 #project chimichanga
+
+class GridPoint:
+	def __init__(self, hasNorthIn, hasSouthIn, hasWestIn, hasEastIn):
+		self.northBool = hasNorthIn
+		self.southBool = hasSouthIn
+		self.westBool = hasWestIn
+		self.eastBool = hasEastIn
+		self.northObj = False
+		self.southObj = False
+		self.westObj = False
+		self.eastObj = False
+	def hasNorth(self):
+		return self.northBool
+	def hasSouth(self):
+		return self.southBool
+	def hasWest(self):
+		return self.westBool
+	def hasEast(self):
+		return self.eastBool
+	def setNorthObj(self, objIn):
+		self.northObj = objIn
+	def getNorthObj(self):
+		return self.northObj
+	def setSouthObj(self, objIn):
+		self.southObj = objIn
+	def getSouthObj(self):
+		return self.southObj
+	def setWestObj(self, objIn):
+		self.westObj = objIn
+	def getWestObj(self):
+		return self.westObj
+	def setEastObj(self, objIn):
+		self.eastObj = objIn
+	def getEastObj(self):
+		return self.eastObj
